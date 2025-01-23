cb25fae6b15e4cc1f13cbbfd94391f4a39b701ff
Asad <m.asadakmal1999@gmail.com>
2025-01-23 14:45:02 +0500

    Redirect to the dashboard on page load and comment out the previous layout structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,26 @@
-"use client";
+
 
 import { AreaComponent } from "@/components/dashboard/areachart";
 import { BarComponent } from "@/components/dashboard/barchart";
 import { ChartComponent } from "@/components/dashboard/chart3";
 import { DashboardStats } from "@/components/dashboard/dashboard-stats";
 import { PieChartMain } from "@/components/dashboard/piechart";
+import { redirect } from 'next/navigation'
+
+
 
 export default function Page() {
+
   return (
-    <div className="container mx-auto mt-8 flex flex-1 flex-col gap-6 p-4 md:p-8">
-      <DashboardStats />
-      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-        <BarComponent />
-        <ChartComponent />
-        <PieChartMain />
-        <AreaComponent />
-      </div>
-    </div>
+    // <div className="container mx-auto mt-8 flex flex-1 flex-col gap-6 p-4 md:p-8">
+    //   <DashboardStats />
+    //   <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+    //     <BarComponent />
+    //     <ChartComponent />
+    //     <PieChartMain />
+    //     <AreaComponent />
+    //   </div>
+    // </div>
+    redirect('/dashboard')
   );
 }
